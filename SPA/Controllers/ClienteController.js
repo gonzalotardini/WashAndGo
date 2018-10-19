f1962102b5346f83794d860a46f5ab101dddf470
gonzalo <gonzalotardini@gmail.com>
2018-10-19 17:17:38 -0300

    se ordena la tabla de lavados cliente

--- SPA/Controllers/ClienteController.js
+++ SPA/Controllers/ClienteController.js
@@ -78,6 +78,13 @@
     };
 
 
+    $scope.sort = function (keyname) {
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    };
+
+    $scope.sort('Fecha');
+
 
     $scope.ObtenerModelos = function (MarcaSeleccionada) {
 
