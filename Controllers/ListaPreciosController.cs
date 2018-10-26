211480f9725b6d3fde491886bddd924d4bcf31ef
gonzalo <gonzalotardini@gmail.com>
2018-10-26 15:28:18 -0300

    se corrigieron muestras de fecha y se maqueto laparte de edit de precios. Ahora se muestra detalle cuando queres ver el detalle del precio

--- Controllers/ListaPreciosController.cs
+++ Controllers/ListaPreciosController.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DAL;
+using PagedList;
+using PagedList.Mvc;
 
 namespace WashAndGo.Controllers
 {
@@ -15,63 +17,106 @@ namespace WashAndGo.Controllers
         private WGentities db = new WGentities();
 
         // GET: ListaPrecios
-        public ActionResult Index(string sortOrder)
+        public ActionResult Index(string sortOrder, string currentFilter ,string search,int? page)
         {
-            ViewBag.FechaDesdeSort = String.IsNullOrEmpty(sortOrder) ? "Fecha_Desde" : "";
-            ViewBag.FechaHastaSort = sortOrder == "Fecha_hasta" ? "Fecha_hasta_desc" : "Fecha_hasta";
-            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
-            ViewBag.PrecioSort = sortOrder == "Precio" ? "Precio_desc" : "Precio";
-            ViewBag.Segmento = sortOrder == "Segmento" ? "Segmento_desc" : "Segmento";
-            ViewBag.Servicios = sortOrder == "Servicios" ? "Serviciose_desc" : "Servicios";
-
-
-            var listaPrecios = db.ListaPrecios.Include(l => l.Segmentos).Include(l => l.Servicios);
-            switch (sortOrder)
+            try
             {
-                case "Fecha_Desde":
-                    listaPrecios = listaPrecios.OrderByDescending(s => s.FechaDesde);
-                    break;
-                case "Fecha_hasta":
-                    listaPrecios = listaPrecios.OrderBy(s => s.FechaHasta);
-                    break;
-                case "Fecha_hasta_Desc":
-                    listaPrecios = listaPrecios.OrderByDescending(s => s.FechaHasta);
-                    break;
-                case "Precio":
-                    listaPrecios = listaPrecios.OrderBy(s => s.Precio);
-                    break;
-                case "Precio_desc":
-                    listaPrecios = listaPrecios.OrderByDescending(s => s.Precio);
-                    break;
-                case "Segmento_desc":
-                    listaPrecios = listaPrecios.OrderByDescending(s => s.Segmentos.Descripcion);
-                    break;
-                case "Segmento":
-                    listaPrecios = listaPrecios.OrderBy(s => s.Segmentos.Descripcion);
-                    break;
-                case "Servicios":
-                    listaPrecios = listaPrecios.OrderBy(s => s.Servicios.Nombre);
-                    break;
-                case "Servicios_desc":
-                    listaPrecios = listaPrecios.OrderByDescending(s => s.Servicios.Nombre);
-                    break;
-                    //default:
-                    //    students = students.OrderBy(s => s.LastName);
-                    //    break;
-            }
+                if (sortOrder==null)
+                {
+                    sortOrder = "Fecha_Desde_desc";
+                }
 
-           
-            return View(listaPrecios.ToList());
+                //ViewBag.FechaDesdeSort = String.IsNullOrEmpty(sortOrder) ? "Fecha_Desde" : "";
+                ViewBag.FechaDesdeSort = sortOrder=="Fecha_Desde" ? "Fecha_Desde_desc": "Fecha_Desde";
+                ViewBag.FechaHastaSort = sortOrder == "Fecha_hasta" ? "Fecha_hasta_desc" : "Fecha_hasta";
+                ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+                ViewBag.PrecioSort = sortOrder == "Precio" ? "Precio_desc" : "Precio";
+                ViewBag.Segmento = sortOrder == "Segmento" ? "Segmento_desc" : "Segmento";
+                ViewBag.Servicios = sortOrder == "Servicios" ? "Servicios_desc" : "Servicios";
+
+
+                if (search != null)
+                {
+                    page = 1;
+                }
+                else
+                {
+                    search = currentFilter;
+                }
+                ViewBag.CurrentFilter = search;
+
+
+                var listaPrecios = db.ListaPrecios.Include(l => l.Segmentos).Include(l => l.Servicios);
+               
+
+                if (!String.IsNullOrEmpty(search))
+                {
+                    listaPrecios = listaPrecios.Where(s => s.FechaDesde.ToString().Contains(search)
+                                           || s.FechaHasta.ToString().Contains(search) || s.Precio.ToString().Contains(search)
+                                            || s.Segmentos.Descripcion.Contains(search) || s.Servicios.Nombre.Contains(search));
+                }
+
+                switch (sortOrder)
+                {
+                    case "Fecha_Desde":
+                        listaPrecios = listaPrecios.OrderBy(s => s.FechaDesde);
+                        break;
+                    case "Fecha_Desde_desc":
+                        listaPrecios = listaPrecios.OrderByDescending(s => s.FechaDesde);
+                        break;
+                    case "Fecha_hasta":
+                        listaPrecios = listaPrecios.OrderBy(s => s.FechaHasta);
+                        break;
+                    case "Fecha_hasta_desc":
+                        listaPrecios = listaPrecios.OrderByDescending(s => s.FechaHasta);
+                        break;
+                    case "Precio":
+                        listaPrecios = listaPrecios.OrderBy(s => s.Precio);
+                        break;
+                    case "Precio_desc":
+                        listaPrecios = listaPrecios.OrderByDescending(s => s.Precio);
+                        break;
+                    case "Segmento_desc":
+                        listaPrecios = listaPrecios.OrderByDescending(s => s.Segmentos.Descripcion);
+                        break;
+                    case "Segmento":
+                        listaPrecios = listaPrecios.OrderBy(s => s.Segmentos.Descripcion);
+                        break;
+                    case "Servicios":
+                        listaPrecios = listaPrecios.OrderBy(s => s.Servicios.Nombre);
+                        break;
+                    case "Servicios_desc":
+                        listaPrecios = listaPrecios.OrderByDescending(s => s.Servicios.Nombre);
+                        break;
+                        //default:
+                        //    students = students.OrderBy(s => s.LastName);
+                        //    break;
+                }
+
+                int pageSize = 10;
+                int pageNumber = (page ?? 1);
+                return View(listaPrecios.ToPagedList(pageNumber, pageSize));
+            }
+            catch (Exception ex) 
+            {
+                return RedirectToAction("Error", "Account");
+                throw;
+            }
+            
         }
 
         // GET: ListaPrecios/Details/5
-        public ActionResult Details(int? id)
+        public ActionResult Details(int? idServicio, int? idSegmento)
         {
-            if (id == null)
+
+            if (idServicio == null || idSegmento == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ListaPrecios listaPrecios = db.ListaPrecios.Find(id);
+
+            ListaPrecios listaPrecios = db.ListaPrecios.Include("Servicios")
+                                                       .Include("Segmentos")
+                                                       .Where(x => x.IdSegmento == idSegmento && x.IdServicio == idServicio).First();
             if (listaPrecios == null)
             {
                 return HttpNotFound();
@@ -109,18 +154,22 @@ namespace WashAndGo.Controllers
         // GET: ListaPrecios/Edit/5
         public ActionResult Edit(int? idServicio, int? idSegmento )
         {
-            var hola = "";
-            //if (id == null)
-            //{
-            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            //}
-            ListaPrecios listaPrecios = db.ListaPrecios.Where(x => x.IdSegmento == idSegmento && x.IdServicio == idServicio).First(); 
+         
+            if (idServicio == null || idSegmento==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ListaPrecios listaPrecios = db.ListaPrecios.Include("Servicios")
+                                                        .Include("Segmentos")
+                                                        .Where(x => x.IdSegmento == idSegmento && x.IdServicio == idServicio).First(); 
            if (listaPrecios == null)
             {
                 return HttpNotFound();
             }
             ViewBag.IdSegmento = new SelectList(db.Segmentos, "IdSegmento", "Descripcion", listaPrecios.IdSegmento);
             ViewBag.IdServicio = new SelectList(db.Servicios, "IdServicio", "Nombre", listaPrecios.IdServicio);
+
+            
             return View(listaPrecios);
         }
 
